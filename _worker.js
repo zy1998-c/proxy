ad8705c151a0c7487e28640c15ff6a1fdb7eab9a
zy1998-c <64115055+zy1998-c@users.noreply.github.com>
2024-03-09 23:47:02 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    url.host = "commentanalyzer.googleapis.com";
+    url.host = "commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1";
     // openai is already set all CORS heasders 
     return fetch(url, {
       headers: request.headers,
